bdf77153a600bb1aa4cdfa364735b55590fefe16
arnab1656 <arnab.paul.1656@gmail.com>
2025-07-09 12:26:26 +0530

    feat: add database connection and core data models

---
+++ src/domain/models/jobQueues.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { IJobQueue } from '../../presentation/interfaces/Mongo_Schemas_Interfaces/JobQueue';
+
+const JobQueueSchema: Schema = new Schema<IJobQueue>(
+  {
+    name: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: Number,
+      required: false,
+    },
+    payload: {
+      type: Object,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: false,
+    },
+    GUID: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const JobQueue = mongoose.model<IJobQueue>('JobQueue', JobQueueSchema);
+
+export default JobQueue;
