fea18b136d32e16bd661f191ad9edbdc126326f0
arnab1656 <arnab.paul.1656@gmail.com>
2025-07-09 12:23:52 +0530

    feat: add core middleware and utility helpers for logging, security, and response formatting

---
+++ src/helpers/jobProcess.ts
@@ -0,0 +1,71 @@
+import { JobQueueRequest } from "../presentation/interfaces/request/JobQueueRequest"
+import logger from "../presentation/middleware/logger"
+import { addJobs } from "../presentation/queue/queue"
+import { queueTypes, queueTypesNames, userPrefrencesTypes } from "../utils/constant"
+import { _sendEmail, sendEmail } from "./notificationsHelper/mail"
+import { getOnce as getUserPref } from "../data-access/repositories/systemPrederencesRepository";
+import twilio from 'twilio';
+import { decrypt } from "../presentation/middleware/security"
+
+export const processEmailJobs = async (job: JobQueueRequest) => {
+    try {
+        if (!job || !job.type) return true
+
+        if (job.type == queueTypes.find(x => x.name == queueTypesNames.notifyOtpEmail)?.id) {
+            let sendMailForOtp = await _sendEmail(
+                job.payload.email,
+                job.payload.template,
+                job.payload.payload
+            ); 
+            if (!sendMailForOtp) return false
+        } else if (
+            job.type == queueTypes.find(x => x.name == queueTypesNames.sensitiveNotification)?.id ||
+            job.type == queueTypes.find(x => x.name == queueTypesNames.confirmationNotification)?.id ||
+            job.type == queueTypes.find(x => x.name == queueTypesNames.informationNotification)?.id
+        ) {
+            let getNotificationCOnfigurations = await getUserPref({ userId: job.userId, type: userPrefrencesTypes.emailSmtp });
+            let smtp = getNotificationCOnfigurations.value;
+            let sendEmailToRecp = await sendEmail({ email: job.payload.recipients.email, emailConfigurations: smtp, message: job.payload.message, subject: job.payload.subject })
+            if (sendEmailToRecp)
+                return true
+            else
+                return false
+
+        }
+        return true
+    } catch (error: any) {
+        logger.error(`error while process job ${error?.message}`)
+        return false
+    }
+}
+export const processSmsJobs = async (job: JobQueueRequest) => {
+    try {
+        if (!job || !job.type) return true
+
+        let getNotificationCOnfigurations = await getUserPref({ userId: job.userId, type: userPrefrencesTypes.twilloSmsConfiguration });
+
+        if (getNotificationCOnfigurations?.value?.accountSid) {
+            const accountSid = await decrypt(getNotificationCOnfigurations?.value?.accountSid);
+            const authToken = await decrypt(getNotificationCOnfigurations?.value?.authToken);
+            const fromPhone = await decrypt(getNotificationCOnfigurations?.value?.fromPhone);
+            const toPhone = job.payload.recipients.phone;
+
+            const client = twilio(accountSid, authToken);
+            let senrm = client.messages
+                .create({
+                    body: job.payload.message,
+                    to: toPhone,
+                    from: fromPhone
+                })
+                .then(message => {console.log('Message sent with SID:', message.sid); logger.info('Message sent with SID:', message.sid)})
+                .catch(error => console.error('Error sending message:', error));
+        }
+        return true
+        // let pushToQueue = await addJobs(job);
+        // if (pushToQueue) return true;
+        // else return false
+    } catch (error: any) {
+        logger.error(`error while process job ${error?.message}`)
+        return false
+    }
+}
---
+++ src/helpers/notificationsHelper/mail.ts
@@ -0,0 +1,121 @@
+import nodemailer from "nodemailer";
+import { emailConfigurations, emailTemplateTypesEnum } from "../../utils/constant";
+import { get } from "../../data-access/repositories/emailTemplatesRepository";
+import logger from "../../presentation/middleware/logger";
+import { emailBodyInterFace } from "../../presentation/interfaces/general/email";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+export const _sendEmail = async (
+    email: any,
+    type: any = "",
+    replacements: any = null
+) => {
+    try {
+        let emailConfiurations = emailConfigurations;
+        let message = await tokenReplace(type, replacements);
+        if (message) {
+            let transporter = nodemailer.createTransport(emailConfigurations.EmailCongigurations);
+            let mailOptions = {
+                // from: emailConfiurations.configurations?.auth?.user,
+                from: `Notification System <${emailConfiurations.EmailCongigurations.auth.user}>`,
+                to: email,
+                cc: emailConfiurations.CC,
+                subject: `${message?.subject || ""}`,
+                html: `<span>${message?.content}</span>`
+            };
+            let info = await transporter.sendMail(mailOptions);
+            console.log("mail sent");
+            return true;
+        } else return false;
+    } catch (error: any) {
+        logger.error("Error occurred:", error.message);
+        return false; // Failed to send email
+    }
+};
+export const sendEmail = async ({ email, message, emailConfigurations, subject }: emailBodyInterFace) => {
+    try {
+        // let emailConfiurations = JSON.parse(emailConfigurations);
+        // let message = message;
+        if (message) {
+            // Create a transporter object using the email configurations
+            let transporter = nodemailer.createTransport({
+                host: emailConfigurations.host,
+                port: emailConfigurations.port,
+                secure: emailConfigurations.secure,
+                auth: {
+                    user: emailConfigurations.auth.user,
+                    pass: emailConfigurations.auth.pass,
+                },
+            } as SMTPTransport.Options);
+            let mailOptions = {
+                // from: emailConfiurations.configurations?.auth?.user,
+                from: `<${emailConfigurations.auth.user}>`,
+                to: email,
+                // cc: emailConfigurations.,
+                subject: `${subject}`,
+                html: `<span>${message}</span>`
+            };
+            let info = await transporter.sendMail(mailOptions);
+            console.log("mail sent");
+            return true;
+        } else return false;
+    } catch (error: any) {
+        logger.error("Error occurred:", error.message);
+        return false; // Failed to send email
+    }
+}
+
+const tokenReplace = async (name: string, replacements: any) => {
+    try {
+        // Fetch template type details
+        const type = emailTemplateTypesEnum.find(x => x.name == name);
+
+        if (!type) {
+            // Handle case where no template type is found
+            console.warn(`Template type with name "${name}" not found.`);
+            return; // Or throw an error if required by your application logic
+        }
+
+        // Fetch template using the retrieved type ID
+        const template: any[] = await get({ typeId: type.id });
+
+        if (template && template?.length > 0) {
+            if (template[0]?.content) {
+                const finalContent = template[0]?.content.replace(
+                    /\{([^\}]+)\}/g,
+                    (match: any, tokenName: any) => {
+                        return replacements[tokenName] !== undefined
+                            ? replacements[tokenName]
+                            : match;
+                    }
+                );
+                const finalSubject = template[0]?.subject.replace(
+                    /\{([^\}]+)\}/g,
+                    (match: any, tokenName: any) => {
+                        return replacements[tokenName] !== undefined
+                            ? replacements[tokenName]
+                            : match;
+                    }
+                );
+                return {
+                    subject: finalSubject,
+                    content: finalContent,
+                };
+            }
+            return {
+                subject: "",
+                content: "",
+            };
+        }
+        return {
+            subject: "",
+            content: "",
+        };
+    } catch (error) {
+        logger.error("Error during token replacement:", error);
+        return {
+            subject: "",
+            content: "",
+        };
+    }
+};
